b3f9b7f8bd8cc68c188610290bf43b5957e61822
erandiganepola <erandiganepola@gmail.com>
2016-07-02 11:41:48 +0530

    Android Assignment

---
+++ app/src/main/java/com/nnt/navod/androidchat/app/EndPoints.java
@@ -0,0 +1,15 @@
+package com.nnt.navod.androidchat.app;
+
+/**
+ * Created by Navod on 7/1/2016.
+ */
+public class EndPoints {
+
+    // localhost url -
+    public static final String BASE_URL = "http://192.168.43.8/gcm_chat/v1";
+    public static final String LOGIN = BASE_URL + "/user/login";
+    public static final String USER = BASE_URL + "/user/_ID_";
+    public static final String CHAT_ROOMS = BASE_URL + "/chat_rooms";
+    public static final String CHAT_THREAD = BASE_URL + "/chat_rooms/_ID_";
+    public static final String CHAT_ROOM_MESSAGE = BASE_URL + "/chat_rooms/_ID_/message";
+}
